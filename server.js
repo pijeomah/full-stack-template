b2430bf19ce8064a16eab01cced23cd223d606ce
Onyedika <promiseij4919@gmail.com>
2024-12-18 02:39:26 +0000

    added connection to Mongo db

--- server.js
+++ server.js
@@ -3,3 +3,14 @@ const app = express()
 const cors = require('cors')
 const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
+let db, 
+    dbConnectionString = process.env.DB_STRING,//declaring a variable from the env file globally
+    dbName='todo',
+    collection
+
+    MongoClient.connect(dbConnectionString)
+    .then(client => {
+        console.log(`Connected to Database`)
+        db = client.db(dbName)
+        collection = db.collection('todos')
+    })
