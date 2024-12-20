262f2d3978828b9ee721e865f31facb4ae74661c
Onyedika <promiseij4919@gmail.com>
2024-12-20 01:29:50 +0000

    added middleware

--- server.js
+++ server.js
@@ -14,3 +14,11 @@ let db,
         db = client.db(dbName)
         collection = db.collection('todos')
     })
+    app.set('view engine', 'ejs')
+    app.use(express.static('public'))
+    app.use(express.urlencoded({extended: true}))
+    app.use(express.json())
+    app.use(cors())
+app.listen(process.env.PORT || PORT, ()=> {
+    console.log(`Server is running on port ${process.env.PORT}`)
+})
